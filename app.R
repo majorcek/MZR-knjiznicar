03ec20915c56fc34a3ea378af453d0c5a0a94be4
majorcek <jasa.stefan@gmail.com>
2020-03-29 16:39:36 +0200

    Dodani dinamični drsniki, omogočeno je uteževanje verjetnosti.

--- app.R
+++ app.R
@@ -1,13 +1,9 @@
 library(shiny)
 library(shinydashboard)
+library(purrr)
 
 source("funkcija.R")
 source("analiza.R")
-
-#source("vizualizacija.R")
-utezi_knjige_in <- c(0.35, 0.23, 0.17, 0.10, 0.05, 0.04, 0.03, 0.03)
-utezi_knjige_out <- c(0.35, 0.23, 0.17, 0.10, 0.05, 0.04, 0.03, 0.03)
-
 
 ui <- dashboardPage(
   dashboardHeader(title = "Projekt knjižničar"),
@@ -29,11 +25,11 @@ ui <- dashboardPage(
       sliderInput(inputId = "max_knjig", label = "kritično št. knjig", 
                   value = 10, min = 1, max = 30),
       sliderInput(inputId = "max_izposojenih", label = "najvecje stevilo knjig izposoje",
-                  value = 8, min = 1, max = 8)
-      #,
-      #uiOutput("ver")
+                  value = 8, min = 1, max = 8),
+      hr(),
+      actionButton(inputId = "posodobi", label = "uporabi nove vrednosti"),
+      uiOutput("izbor_knjig")
       
-      #numericInput(inputId = "verjetnost1", label = "P(vzame 1 knjigo)", value = 1, min = 0, max = 1, step = 0.01),
     )
   ),
   
@@ -42,54 +38,94 @@ ui <- dashboardPage(
       tabPanel("ŠTEVILO STRANK V KNJIŽNICI", plotOutput("graf_stranke_vedno")),
       tabPanel("ČAS BREZDELJA", plotOutput("graf_brezdelje")),
       tabPanel("PODATKI", 
-               fluidRow(column(4,
-                               wellPanel("Skupno število strank: ", verbatimTextOutput("skupno_strank"),
-                                         "Stranke glede na vrsto opravila"))),
-               hr(),
-               fluidRow(column(4,
+               fluidRow(column(7,
+                               plotOutput("graf_vrste_strank")),
+                        column(5,
+                               wellPanel("Skupno število strank: ", verbatimTextOutput("skupno_strank")),
+                               wellPanel("Skupno število prinešenih knjig: ", verbatimTextOutput("skupno_knjige")),
                                wellPanel("Čas, ko knjižničar zaključi z delom: ", verbatimTextOutput("zaklepanje")))),
+                        
                hr(),
-               fluidRow(column(4,
-                               wellPanel("Skupno število prinešenih knjig: ", verbatimTextOutput("skupno_knjige"))),
-                        column(4,
-                               wellPanel("Skupni čas čakanja:" , verbatimTextOutput("skupno_cakanje"),
-                                         "Najdaljše čakanje: ", verbatimTextOutput("max_cakanje"))),
-                        column(4,
-                               wellPanel("Dolžina najdaljšega opravka v knjižnici: ", verbatimTextOutput("max_cas_knjiznica"))))
+               fluidRow(column(2,
+                               wellPanel("Skupni čas čakanja:" , verbatimTextOutput("skupno_cakanje"))),
+                                         
+                        column(5,
+                               plotOutput("graf_cakanje"),
+                               "Dolzina najdaljšega čakanje na strežbo: ", verbatimTextOutput("max_cakanje_na_strezbo")),
+                        column(5,
+                               plotOutput("graf_v_knjiznici"),
+                               "Dolžina najdaljšega opravka v knjižnici: ", verbatimTextOutput("max_cas_v_knjiznici")))
+               
       )
     )
   )
 )
 
 
-
-
-server <- function(input,output){
-  
-  # vrednost <- reactiveValues(
-  #   stevilo = 0
-  # )
-  # 
-  # observeEvent(input$max_izposojenih,
-  #              {
-  #                vrednost$stevilo <- input$max_izposojenih
-  #              })
-  # 
-  # output$ver <- renderUI({
-  #   vrednost$stevilo
-  #   })
-  # 
-  #output$verjetnost1 <- renderUI({numericInput(inputId = "verjetnost2", "Izberi P(k = 2)", value = 0, min = 0, max = 1 - input$verjetnost1)})
-  
-  tabela <- reactive({ustvari_skupno_tabelo(input$parameter_prihodi, 
-                                            input$parameter_klici, 
-                                            utezi_knjige_in, 
-                                            utezi_knjige_out, 
-                                            input$cas_obratovanja, 
-                                            input$max_knjig,
-                                            input$klici_ali,
-                                            8)
+server <- function(input,output,session){
+  col_names <- reactive(paste0("knjige_input", seq_len(input$max_izposojenih)))
+  output$izbor_knjig <- renderUI({
+    map(col_names(), ~ sliderInput(.x , .x, min = 0, max = 1, value = 0, step = 0.01))
   })
+  output$imena <- renderPrint({print(length(col_names()))})
+  
+  utezi <- reactive({
+    c(input[["knjige_input1"]], input[["knjige_input2"]], input[["knjige_input3"]], input[["knjige_input4"]],
+      input[["knjige_input5"]], input[["knjige_input6"]], input[["knjige_input7"]], input[["knjige_input8"]])[1:input[["max_izposojenih"]]]
+  })
+  
+  vsota_utezi <- reactive({sum(utezi())})
+  
+  observeEvent(input$max_izposojenih,{
+    updateSliderInput(session, inputId = "knjige_input1", value = 1)
+    updateSliderInput(session, inputId = "knjige_input2", value = 0)
+    updateSliderInput(session, inputId = "knjige_input3", value = 0)
+    updateSliderInput(session, inputId = "knjige_input4", value = 0)
+    updateSliderInput(session, inputId = "knjige_input5", value = 0)
+    updateSliderInput(session, inputId = "knjige_input6", value = 0)
+    updateSliderInput(session, inputId = "knjige_input7", value = 0)
+    updateSliderInput(session, inputId = "knjige_input8", value = 0)
+  })
+  
+  vrednost1 <- reactive({input$knjige_input1})
+  vrednost2 <- reactive({input$knjige_input2})
+  vrednost3 <- reactive({input$knjige_input3})
+  vrednost4 <- reactive({input$knjige_input4})
+  vrednost5 <- reactive({input$knjige_input5})
+  vrednost6 <- reactive({input$knjige_input6})
+  vrednost7 <- reactive({input$knjige_input7})
+  vrednost8 <- reactive({input$knjige_input8})
+  
+  observeEvent(input$posodobi,{
+    # 1. posodobimo verjetnost
+    updateSliderInput(session, inputId = "knjige_input1", value = vrednost1() / vsota_utezi())
+    updateSliderInput(session, inputId = "knjige_input2", value = vrednost2() / vsota_utezi())
+    updateSliderInput(session, inputId = "knjige_input3", value = vrednost3() / vsota_utezi())
+    updateSliderInput(session, inputId = "knjige_input4", value = vrednost4() / vsota_utezi())
+    updateSliderInput(session, inputId = "knjige_input5", value = vrednost5() / vsota_utezi())
+    updateSliderInput(session, inputId = "knjige_input6", value = vrednost6() / vsota_utezi())
+    updateSliderInput(session, inputId = "knjige_input7", value = vrednost7() / vsota_utezi())
+    updateSliderInput(session, inputId = "knjige_input8", value = vrednost8() / vsota_utezi())
+  })
+  
+  
+  tabela <- eventReactive(c(input$parameter_prihodi, 
+                            input$parameter_klici, 
+                            input$posodobi,
+                            input$cas_obratovanja, 
+                            input$max_knjig,
+                            input$klici_ali,
+                            input$max_izposojenih),
+                          
+                          {ustvari_skupno_tabelo(input$parameter_prihodi, 
+                                                 input$parameter_klici, 
+                                                 utezi(), 
+                                                 utezi(), 
+                                                 input$cas_obratovanja, 
+                                                 input$max_knjig,
+                                                 input$klici_ali,
+                                                 input$max_izposojenih)
+                          })
   
   output$graf_stranke_vedno <- renderPlot({
     tabela_stranke_prihodi <- naredi_tabelo_stanja_strank(tabela())
@@ -99,19 +135,43 @@ server <- function(input,output){
   })
   
   output$graf_brezdelje <- renderPlot({
-    tabela_brezdelje <- naredi_tabelo_brez_dela(tabela(), tail(tabela()$cas_odhoda,1)+ + tail(tabela()$cas_knjiznicarja,1))
+    tabela_brezdelje <- naredi_tabelo_brez_dela(tabela(), tail(tabela()$cas_odhoda,1) + tail(tabela()$cas_knjiznicarja,1))
     ggplot(data = tabela_brezdelje) + 
       geom_point(mapping = aes(x = cas, y = cas_brez_dela)) +
       geom_line(mapping = aes(x = cas, y = cas_brez_dela)) +
       scale_x_continuous(breaks=seq(0, tail(tabela()$cas_odhoda,1) + tail(tabela()$cas_knjiznicarja,1), ((tail(tabela()$cas_odhoda,1) + tail(tabela()$cas_knjiznicarja,1))%/%1000) * 100))
   })
   
-  output$skupno_strank <- renderPrint({as.integer(podatki(tabela())[1])})
-  output$zaklepanje <- renderPrint({podatki(tabela())[[2]]})
-  output$skupno_knjige <- renderPrint({podatki(tabela())[[3]]})
-  output$skupno_cakanje <- renderPrint({podatki(tabela())[[4]]})
-  output$max_cakanje <- renderPrint({podatki(tabela())[[5]]})
-  output$max_cas_knjiznica <- renderPrint({podatki(tabela())[[6]]})
+  
+  podatki <- reactive({pridobi_podatke(tabela())})
+  
+  output$skupno_strank <- renderPrint({as.integer(podatki()[1])})
+  output$zaklepanje <- renderPrint({podatki()[[2]]})
+  output$skupno_knjige <- renderPrint({podatki()[[3]]})
+  output$skupno_cakanje <- renderPrint({podatki()[[4]]})
+  output$max_cakanje_na_strezbo <- renderPrint({podatki()[[5]]})
+  output$max_cas_v_knjiznici <- renderPrint({podatki()[[6]]})
+
+    output$graf_vrste_strank <- renderPlot({
+    tabela_stranke_opravila <- tabela() %>% group_by(vrsta_opravila) %>% count()
+    ggplot(data = tabela_stranke_opravila) +
+      geom_bar(width = 1, aes(x = "", y = n, fill=vrsta_opravila), stat = "identity") +
+      coord_polar("y", start=0) +
+      scale_fill_manual(values=list("#999999", "#E69F00", "#56B4E9", "#6C8029"))
+  })
+  output$graf_v_knjiznici <- renderPlot({
+    tabela_v_knjiznici <- data.frame(tabela()$vrstni_red, tabela()$cas_odhoda - tabela()$cas_prihoda)
+    names(tabela_v_knjiznici) <- c("vrstni red", "cas v knjiznici")
+    ggplot(data = tabela_v_knjiznici) +
+      geom_col(aes(x = `vrstni red`, y = `cas v knjiznici`))
+  })
+  
+  output$graf_cakanje <- renderPlot({
+    tabela_cakanje <- data.frame(tabela()$vrstni_red, tabela()$cas_zacetka_strezbe - tabela()$cas_prihoda)
+    names(tabela_cakanje) <- c("vrstni red", "cas cakanja")
+    ggplot(data = tabela_cakanje) +
+      geom_col(aes(x = `vrstni red`, y = `cas cakanja`))
+  })
 }
 
 shinyApp(ui = ui, server = server)
