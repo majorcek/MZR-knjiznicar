1e49a5e580c63baf4425ce3ce0ba9060a71be555
majorcek <jasa.stefan@gmail.com>
2020-04-16 21:59:05 +0200

    mini spremembe

--- app.R
+++ app.R
@@ -40,47 +40,47 @@ ui <- dashboardPagePlus(
                                 title = "Projekt knjižničar", 
                                 enable_rightsidebar = FALSE),
   sidebar <- dashboardSidebar(width = 280,
-    sidebarMenu(
-                hr(),
-                
-                menuItem("PRIHODI", icon = icon("sort", class = NULL, lib = "font-awesome"),
-                         numericInput(inputId = "parameter_prihodi", label = "intenziteta prihodov", value = 0.025, min = 0.001, max = 1, step = 0.001),
-                         checkboxInput(inputId = "klici_ali", label = "klici dovoljeni", value = TRUE, width = 60),
-                         conditionalPanel(
-                           condition = "input.klici_ali == true",
-                           numericInput(inputId = "parameter_klici", label = "intenziteta klicov",
-                                        value = 0.01, min = 0.001, max = 1, step = 0.001, width = 180)
-                                  )
-                         ),
-                
-                menuItem("DELOVNI ČAS", icon = icon("clock", lib = "font-awesome"), 
-                         sliderInput(inputId = "cas_obratovanja", label = "Čas obratovanja", 
-                            value = as.numeric(20000), min = 3600, max = 43200)
-                         ),
-                
-                menuItem("OMEJITEV KNJIŽNIČARJA", icon = icon("book", class = NULL, lib = "font-awesome"), 
-                         sliderInput(inputId = "max_knjig", label = "kritično št. knjig", 
-                            value = 10, min = 1, max = 30)
-                         ),
-                
-                menuItem("OMEJITEV IZPOSOJE", icon = icon("id-card", class = NULL, lib = "font-awesome"), 
-                         sliderInput(inputId = "max_izposojenih", label = "najvecje stevilo knjig izposoje",
-                            value = 8, min = 1, max = 8)
-                         ),
-                
-                hr(),
-                
-                actionButton(inputId = "posodobi", label = "",
-                             icon = icon("sync-alt", class = NULL, lib = "font-awesome")),
-                
-                hr(),
-                
-                menuItem("UTEŽI", icon = icon("list-ol", class = NULL, lib = "font-awesome"), 
-                         uiOutput("prvi_drsnik"),
-                         uiOutput("ostali_drsniki")
-                         )
-                
-    )
+                              sidebarMenu(
+                                hr(),
+                                
+                                menuItem("PRIHODI", icon = icon("sort", class = NULL, lib = "font-awesome"),
+                                         numericInput(inputId = "parameter_prihodi", label = "intenziteta prihodov", value = 0.025, min = 0.001, max = 1, step = 0.001),
+                                         checkboxInput(inputId = "klici_ali", label = "klici dovoljeni", value = TRUE, width = 60),
+                                         conditionalPanel(
+                                           condition = "input.klici_ali == true",
+                                           numericInput(inputId = "parameter_klici", label = "intenziteta klicov",
+                                                        value = 0.01, min = 0.001, max = 1, step = 0.001, width = 180)
+                                         )
+                                ),
+                                
+                                menuItem("DELOVNI ČAS", icon = icon("clock", lib = "font-awesome"), 
+                                         sliderInput(inputId = "cas_obratovanja", label = "Čas obratovanja", 
+                                                     value = as.numeric(20000), min = 3600, max = 43200)
+                                ),
+                                
+                                menuItem("OMEJITEV KNJIŽNIČARJA", icon = icon("book", class = NULL, lib = "font-awesome"), 
+                                         sliderInput(inputId = "max_knjig", label = "kritično št. knjig", 
+                                                     value = 10, min = 1, max = 30)
+                                ),
+                                
+                                menuItem("OMEJITEV IZPOSOJE", icon = icon("id-card", class = NULL, lib = "font-awesome"), 
+                                         sliderInput(inputId = "max_izposojenih", label = "najvecje stevilo knjig izposoje",
+                                                     value = 8, min = 1, max = 8)
+                                ),
+                                
+                                hr(),
+                                
+                                actionButton(inputId = "posodobi", label = "",
+                                             icon = icon("sync-alt", class = NULL, lib = "font-awesome")),
+                                
+                                hr(),
+                                
+                                menuItem("UTEŽI", icon = icon("list-ol", class = NULL, lib = "font-awesome"), 
+                                         uiOutput("prvi_drsnik"),
+                                         uiOutput("ostali_drsniki")
+                                )
+                                
+                              )
   ),
   
   body <- dashboardBody(
@@ -89,40 +89,40 @@ ui <- dashboardPagePlus(
       .irs-slider {width: 30px; height: 20px; top: 20px;}
     "),
     tabsetPanel(type = "pills",
-      tabPanel("STANJE V KNJIŽNICI", 
-               withSpinner(plotOutput("graf_stranke_vedno"), type = 2, color = "grey", color.background = 'lightblue'),
-               withSpinner(plotOutput("graf_brezdelje"), type = 2, color = "grey", color.background = 'lightblue')),
-      tabPanel("PODATKI", 
-               fluidRow(column(7,
-                               plotOutput("graf_vrste_strank")),
-                        column(5,
-                               wellPanel("Skupno število strank: ", verbatimTextOutput("skupno_strank")),
-                               wellPanel("Skupno število prinešenih knjig: ", verbatimTextOutput("skupno_knjige")),
-                               wellPanel("Čas, ko knjižničar zaključi z delom: ", verbatimTextOutput("zaklepanje")))),
-               
-               hr(),
-               fluidRow(column(2,
-                               wellPanel("Skupni čas čakanja:" , verbatimTextOutput("skupno_cakanje"))),
-                        
-                        column(5,
-                               plotOutput("graf_cakanje"),
-                               "Dolzina najdaljšega čakanje na strežbo: ", verbatimTextOutput("max_cakanje_na_strezbo")),
-                        column(5,
-                               plotOutput("graf_v_knjiznici"),
-                               "Dolžina najdaljšega opravka v knjižnici: ", verbatimTextOutput("max_cas_v_knjiznici"))
-                        )
-               ),
-      tabPanel("PRIMERJAVA",
-               fluidRow(column(6, 
-                               wellPanel(plotOutput("graf_knjige_izposojene"),
-                                         verbatimTextOutput("delezi_izposojene"))),
-                        
-                        column(6,
-                               wellPanel(plotOutput("graf_knjige_vrnjene"),
-                                         verbatimTextOutput("delezi_vrnjene")))
-                        ),
-               fluidRow(verbatimTextOutput("utezi_in"))
-      )
+                tabPanel("STANJE V KNJIŽNICI", 
+                         withSpinner(plotOutput("graf_stranke_vedno"), type = 2, color = "grey", color.background = 'lightblue'),
+                         withSpinner(plotOutput("graf_brezdelje"), type = 2, color = "grey", color.background = 'lightblue')),
+                tabPanel("PODATKI", 
+                         fluidRow(column(7,
+                                         plotOutput("graf_vrste_strank")),
+                                  column(5,
+                                         wellPanel("Skupno število strank: ", verbatimTextOutput("skupno_strank")),
+                                         wellPanel("Skupno število prinešenih knjig: ", verbatimTextOutput("skupno_knjige")),
+                                         wellPanel("Čas, ko knjižničar zaključi z delom: ", verbatimTextOutput("zaklepanje")))),
+                         
+                         hr(),
+                         fluidRow(column(2,
+                                         wellPanel("Skupni čas čakanja:" , verbatimTextOutput("skupno_cakanje"))),
+                                  
+                                  column(5,
+                                         plotOutput("graf_cakanje"),
+                                         "Dolzina najdaljšega čakanje na strežbo: ", verbatimTextOutput("max_cakanje_na_strezbo")),
+                                  column(5,
+                                         plotOutput("graf_v_knjiznici"),
+                                         "Dolžina najdaljšega opravka v knjižnici: ", verbatimTextOutput("max_cas_v_knjiznici"))
+                         )
+                ),
+                tabPanel("PRIMERJAVA",
+                         fluidRow(column(6, 
+                                         wellPanel(plotOutput("graf_knjige_izposojene"),
+                                                   verbatimTextOutput("delezi_izposojene"))),
+                                  
+                                  column(6,
+                                         wellPanel(plotOutput("graf_knjige_vrnjene"),
+                                                   verbatimTextOutput("delezi_vrnjene")))
+                         ),
+                         fluidRow(verbatimTextOutput("utezi_in"))
+                )
     )
   ),
   rightsidebar = rightSidebar()
@@ -165,13 +165,13 @@ server <- function(input, output, session){
                             updateSliderInput(session, inputId = "knjige_input8", value = input$knjige_input8 / vsota_utezi)                          
                             
                             ustvari_skupno_tabelo(input$parameter_prihodi, 
-                                                 input$parameter_klici, 
-                                                 c(input$knjige_input1, input$knjige_input2, input$knjige_input3, input$knjige_input4, input$knjige_input5, input$knjige_input6, input$knjige_input7, input$knjige_input8)[1:input$max_izposojenih], 
-                                                 c(input$knjige_input1, input$knjige_input2, input$knjige_input3, input$knjige_input4, input$knjige_input5, input$knjige_input6, input$knjige_input7, input$knjige_input8)[1:input$max_izposojenih], 
-                                                 input$cas_obratovanja, 
-                                                 input$max_knjig,
-                                                 input$klici_ali,
-                                                 input$max_izposojenih)
+                                                  input$parameter_klici, 
+                                                  c(input$knjige_input1, input$knjige_input2, input$knjige_input3, input$knjige_input4, input$knjige_input5, input$knjige_input6, input$knjige_input7, input$knjige_input8)[1:input$max_izposojenih], 
+                                                  c(input$knjige_input1, input$knjige_input2, input$knjige_input3, input$knjige_input4, input$knjige_input5, input$knjige_input6, input$knjige_input7, input$knjige_input8)[1:input$max_izposojenih], 
+                                                  input$cas_obratovanja, 
+                                                  input$max_knjig,
+                                                  input$klici_ali,
+                                                  input$max_izposojenih)
                           })
   
   output$graf_stranke_vedno <- renderPlot({
@@ -179,7 +179,7 @@ server <- function(input, output, session){
     ggplot(data = tabela_stranke_prihodi) +
       geom_step(mapping = aes(x = cas, y = stevilo_strank_v_knjiznici)) +
       scale_x_continuous(breaks=seq(0, tail(tabela()$cas_odhoda,1) + tail(tabela()$cas_knjiznicarja,1), ((tail(tabela()$cas_odhoda,1) + tail(tabela()$cas_knjiznicarja,1))%/%1000) * 100))
-    })
+  })
   
   output$graf_brezdelje <- renderPlot({
     tabela_brezdelje <- naredi_tabelo_brez_dela(tabela(), tail(tabela()$cas_odhoda,1) + tail(tabela()$cas_knjiznicarja,1))
@@ -193,12 +193,12 @@ server <- function(input, output, session){
   
   podatki <- reactive({pridobi_podatke(tabela())})
   
-  output$skupno_strank <- renderPrint({as.integer(podatki()[1])})
-  output$zaklepanje <- renderPrint({podatki()[[2]]})
-  output$skupno_knjige <- renderPrint({podatki()[[3]]})
-  output$skupno_cakanje <- renderPrint({podatki()[[4]]})
-  output$max_cakanje_na_strezbo <- renderPrint({podatki()[[5]]})
-  output$max_cas_v_knjiznici <- renderPrint({podatki()[[6]]})
+  output$skupno_strank <- renderText({as.integer(podatki()[1])})
+  output$zaklepanje <- renderText({podatki()[[2]]})
+  output$skupno_knjige <- renderText({podatki()[[3]]})
+  output$skupno_cakanje <- renderText({podatki()[[4]]})
+  output$max_cakanje_na_strezbo <- renderText({podatki()[[5]]})
+  output$max_cas_v_knjiznici <- renderText({podatki()[[6]]})
   
   output$graf_vrste_strank <- renderPlot({
     tabela_stranke_opravila <- tabela() %>% group_by(vrsta_opravila) %>% count()
@@ -226,8 +226,8 @@ server <- function(input, output, session){
   
   
   output$graf_knjige_izposojene <- renderPlot({
-      ggplot(data = izposoja_knjig(tabela())) +
-        geom_col(aes(x = st_izposojenih_knjig, y = n))
+    ggplot(data = izposoja_knjig(tabela())) +
+      geom_col(aes(x = st_izposojenih_knjig, y = n))
   })
   
   output$graf_knjige_vrnjene <- renderPlot({
